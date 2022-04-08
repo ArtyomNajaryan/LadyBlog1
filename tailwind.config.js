72799a98abcc36912d77dd59b0970187d5faef91
ArtyomNajaryan <najaryan97@mail.ru>
2022-04-08 17:16:14 +0400

    fghfghgfh

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,5 @@ module.exports = {
   plugins: [
     require('tw-elements/dist/plugin')
   ],
-  theme: {
-    container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-    },
-  },
+
 }
