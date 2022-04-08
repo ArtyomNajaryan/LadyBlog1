6fb65a55fe61c00ac328faad0223100d8a670c81
ArtyomNajaryan <najaryan97@mail.ru>
2022-04-08 16:10:37 +0400

    Container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
     './*.{html,js}',],
   plugins: [
     require('tw-elements/dist/plugin')
-  ]
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
 }
